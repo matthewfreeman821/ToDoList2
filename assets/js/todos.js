8aa5c7b3dd5346a8095dd13eb3bee30f4cf5fd12
Matthew Freeman <matthew.freeman821@gmail.com>
2018-09-24 14:07:03 -0400

    Updated to include font awesome and updated js file to include icons

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -19,6 +19,6 @@ $("input[type='text']").keypress(function(event){
         var toDoText = ($(this).val());
         $(this).val('');
         //create a new li and add to ul
-        $('ul').append('<li><span>X</span> ' + toDoText +'</li>');
+        $('ul').append('<li><span><i class="fas fa-trash-alt"></i></span> ' + toDoText +'</li>');
     }
 });
