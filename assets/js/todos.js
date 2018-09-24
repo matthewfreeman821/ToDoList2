fe8ab122ec7336b6987cc4df6551ec5abc9dd829
Matthew Freeman <matthew.freeman821@gmail.com>
2018-09-24 13:34:47 -0400

    Added functionality to allow adding, removing, and marking Todo items

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,24 @@
 // Check off a specific todo by click
-$('li').click(function(){
+//The on is used so this fucntion works on future LI's, but is added to the UL because it will not work on future LI's.
+//This is why the LI is passed into the .on() function, this is because the UL has been created when the page loads, therefore it will apply to future LI's created in the UL
+$('ul').on('click', 'li', function(){
     $(this).toggleClass('completed');
-})
+});
 
 // Click on X to delete todo
-$('span').click(function(event){
+$('ul').on('click', 'span', function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();//prevents bubbling into the li click function
-})
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){//13 is the key code for the enter key
+        //saving new todo text from input box
+        var toDoText = ($(this).val());
+        $(this).val('');
+        //create a new li and add to ul
+        $('ul').append('<li><span>X</span> ' + toDoText +'</li>');
+    }
+});
